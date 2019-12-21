6fc892b720f80173fdeaa98819897ef8098a58e1
Benedikt Strobel <benedikt.strobel@tum.de>
2019-12-21 15:26:56 +0100

    Initial Commit

---
+++ WhosHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhosHome
+//
+//  Created by user162931 on 12/21/19.
+//  Copyright © 2019 Benedikt Strobel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
